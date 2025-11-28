b10a6c15745d514fd7087785a2836b8190d0119f
Will Davis <willdavis9005@Gmail.com>
2025-11-28 16:29:07 -0600

    Initial commit

---
+++ Player/Scripts/player.gd
@@ -0,0 +1,18 @@
+class_name Player extends CharacterBody2D
+
+var move_speed : float = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction : Vector2 = Vector2.ZERO
+	direction.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	direction.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
+	velocity = direction * move_speed
+	
+func _physics_process(delta):
+	move_and_slide()
