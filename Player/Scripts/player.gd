18f98cc692cdaf3d39a0e03d6fe32faa40d47588
Will Davis <willdavis9005@Gmail.com>
2025-11-29 01:07:07 -0600

    Added animation

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,6 +1,12 @@
 class_name Player extends CharacterBody2D
 
+var direction : Vector2 = Vector2.ZERO
 var move_speed : float = 100
+var cardinal_direction : Vector2 = Vector2.DOWN
+var state : String = "idle"
+
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var sprite : Sprite2D = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,10 +15,42 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var direction : Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	direction.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
-	velocity = direction * move_speed
+	velocity = direction * move_speed	
+	if SetState() == true || SetDirection() == true: UpdateAnimation()
+	pass
 	
 func _physics_process(delta):
 	move_and_slide()
+
+func SetDirection() -> bool:
+	var new_direction : Vector2 = cardinal_direction
+	if direction == Vector2.ZERO: return false
+	if direction.y == 0:
+		new_direction = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_direction = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	if new_direction == cardinal_direction:
+		return false
+	cardinal_direction	= new_direction
+	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
+	return true;
+
+func SetState() -> bool:
+	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
+	if new_state == state: return false
+	state = new_state
+	return true;
+	
+func UpdateAnimation() -> void:
+	animation_player.play(state + "_" + AnimDirection())
+	pass
+
+func AnimDirection() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple Godot project that I'm making for fun.
